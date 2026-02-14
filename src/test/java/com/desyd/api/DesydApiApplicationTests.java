e775e057436e0c8191c91740dc42592903bb6065
Mahir <itsmahir24@gmail.com>
2026-02-14 13:46:26 +0200

    Initial commit with file structure

---
+++ src/test/java/com/desyd/api/DesydApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desyd.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesydApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
