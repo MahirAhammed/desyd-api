7756ba3fc23cea7d997898c01187100e48d63f82
Mahir <itsmahir24@gmail.com>
2026-02-19 19:04:20 +0200

    Created jpa repository for User and UserProfile

---
+++ src/main/java/com/desyd/api/repository/UserProfileRepository.java
@@ -0,0 +1,16 @@
+package com.desyd.api.repository;
+
+import com.desyd.api.entity.UserProfile;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface UserProfileRepository extends JpaRepository<UserProfile, UUID> {
+
+    Optional<UserProfile> findByUsername(String username);
+    boolean existsByUsername(String username);
+
+}
