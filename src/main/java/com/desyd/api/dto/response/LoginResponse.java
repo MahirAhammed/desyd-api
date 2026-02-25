e1a92479bb56d42bca7368d3c4c68ec155a8539c
Mahir <itsmahir24@gmail.com>
2026-02-25 10:29:57 +0200

    feat: Added authentication request and response DTOs

---
+++ src/main/java/com/desyd/api/dto/response/LoginResponse.java
@@ -0,0 +1,39 @@
+package com.desyd.api.dto.response;
+
+public class LoginResponse {
+
+    private String accessToken;
+    private String refreshToken;
+    private String tokenType = "Bearer";
+    private Long expiresIn;
+    private UserProfileResponse user;
+
+    public LoginResponse(){}
+
+    public LoginResponse(String accessToken, String refreshToken, Long expiresIn, UserProfileResponse user) {
+        this.accessToken = accessToken;
+        this.refreshToken = refreshToken;
+        this.expiresIn = expiresIn;
+        this.user = user;
+    }
+
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+
+    public void setRefreshToken(String refreshToken) {
+        this.refreshToken = refreshToken;
+    }
+
+    public void setTokenType(String tokenType) {
+        this.tokenType = tokenType;
+    }
+
+    public void setExpiresIn(Long expiresIn) {
+        this.expiresIn = expiresIn;
+    }
+
+    public void setUser(UserProfileResponse user) {
+        this.user = user;
+    }
+}
