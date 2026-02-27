18cc5c1ec39a3aa246718b851a22add0312c4db0
Mahir <itsmahir24@gmail.com>
2026-02-27 15:53:34 +0200

    feat: Included queries to update user session stats

--- src/main/java/com/desyd/api/repository/UserProfileRepository.java
+++ src/main/java/com/desyd/api/repository/UserProfileRepository.java
@@ -2,6 +2,9 @@ package com.desyd.api.repository;
 
 import com.desyd.api.entity.UserProfile;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -12,5 +15,13 @@ public interface UserProfileRepository extends JpaRepository<UserProfile, UUID> 
 
     Optional<UserProfile> findByUsername(String username);
     boolean existsByUsername(String username); // TODO: username exists case insensitive and only differs by 2 characters
+
+    @Modifying
+    @Query("UPDATE UserProfile SET sessionsHosted = sessionsHosted + 1 WHERE userId = :id")
+    void incrementSessionsHosted(@Param("id") UUID id);
+
+    @Modifying
+    @Query("UPDATE UserProfile SET sessionsJoined = sessionsJoined + 1 WHERE userId = :id")
+    void incrementSessionsJoined(@Param("id") UUID id);
 
 }
